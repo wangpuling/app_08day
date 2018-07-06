07c5afd26cdced1bfd2bbc6b19b746e7f004778a
wangpuling <13121853037@163.com>
2018-07-06 20:00:26 +0800

    添加严重级别

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,8 +1,10 @@
-import allure
+import allure, pytest
 
 class Test_001_01:
 
+    @pytest.allure.severtity(pytest.allure.servrity_level.BLOCKER)
     @allure.step(title="第一个测试")
     def test_001(self):
         allure.attach('这是一个描述内容')
         assert 0
+
