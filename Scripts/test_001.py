137f13d27fb56d7976746e39ee42257d8aaee3d3
wangpuling <13121853037@163.com>
2018-07-06 20:04:23 +0800

    添加严重级别1

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -2,7 +2,7 @@ import allure, pytest
 
 class Test_001_01:
 
-    @pytest.allure.severtity(pytest.allure.servrity_level.BLOCKER)
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title="第一个测试")
     def test_001(self):
         allure.attach('这是一个描述内容')
